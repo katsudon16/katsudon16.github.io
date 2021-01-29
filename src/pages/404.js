5932550ec66172eb373a68569c8d52519c6e3d6a
katsudon16 <sucipto.kathleen@gmail.com>
2021-01-29 16:39:41 -0500

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { palette } from '../components/const';
 import styled from 'styled-components';
 import { HomeSVG } from '../components/svgs/navbar';
@@ -14,23 +14,58 @@ const Container = styled.div`
     align-items: center;
 `
 const Content = styled.div`
-    width: 60vh;
+    width: 80vh;
     text-align: center;
 
-    h1 {
-        font-size: 90px;
-        color: ${palette.white};
-        font-family: Allura;
+    .mainTitle {
+        position: relative;
+        height: 300px;
+        
+        h1 {
+            top: -60px;
+            width: 100%;
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 120px;
+            margin: 0;
+            color: ${palette.white};
+            font-family: Allura;
+            position: absolute;
+            opacity: 0;
+            transition: opacity 1s, top 1s;
+            
+            &.visible {
+                top: 0;
+                opacity: 1;
+            }
+    
+        }
     }
+
     h3 {
+        opacity: 0;
         font-size: 30px;
         color: ${palette.bgContrast};
+        transition: opacity 1s;
+
+        &.visible {
+            opacity: 1;
+        }
     }
     a {
+        opacity: 0;
+        transition: opacity 1s;
+        &.visible {
+            opacity: 1;
+        }
         #iconHome {
             height: 100px;
             width: 100px;
             stroke: ${palette.white};
+
+            &.visible {
+                opacity: 1;
+            }
             &:hover {
                 cursor: pointer;
                 stroke: ${palette.bgContrast};
@@ -38,14 +73,44 @@ const Content = styled.div`
         }
     }
 `
-const Home = () => (
-    <Container>
-        <Content>
-            <h1>Oops,<br />page not found!</h1>
-            <h3>Go back home?</h3>
-            <a href='/' aria-label='Go back to home page'><HomeSVG /></a>
-        </Content>
-    </Container>
-);
+class NotFoundPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mainTitleVisible: false,
+            titleVisible: false,
+            homeVisible: false,
+        };
+    }
+    componentDidMount() {
+        const timeout = {
+            mainTitleVisible: 200,
+            titleVisible: 300,
+            homeVisible: 500,
+        };
+        for (let key in timeout) {
+            setTimeout(() => {
+                this.setState({ [key]: true });
+            }, timeout[key]);
+        }
+    }
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <div className='mainTitle'>
+                        <h1 className={this.state.mainTitleVisible ? 'visible' : ''}>
+                            Oops,<br />page not found!
+                        </h1>
+                    </div>
+                    <h3 className={this.state.titleVisible ? 'visible' : ''}>Go back to the home page?</h3>
+                    <a href='/' aria-label='Go back to home page' className={this.state.homeVisible ? 'visible' : ''}>
+                        <HomeSVG />
+                    </a>
+                </Content>
+            </Container>
+        );
+    }
+}
 
-export default Home;
+export default NotFoundPage;
